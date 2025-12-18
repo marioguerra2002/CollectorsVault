72198e37ad01c7e0c60bac469e141a335a03e06d
marioguuerra2002 <alu0101395036@ull.edu.es>
2025-12-18 02:38:15 +0000

    card grid problem solved

--- client/src/app/components/CardGridExploration.tsx
+++ client/src/app/components/CardGridExploration.tsx
@@ -1,5 +1,12 @@
 'use client';
-import Image from 'next/image';
+interface Card {
+  id: string;
+  name?: string;
+  value: number;
+  imageUrl?: string;
+  isTradable?: boolean; 
+  condition?: string;
+}
 interface CardGridProps {
   cards: Card[];
   onCardClick: (cardId: string) => void;
