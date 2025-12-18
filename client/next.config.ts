7626ecb62a59890576f188442e9df04a882e76d3
marioguuerra2002 <alu0101395036@ull.edu.es>
2025-12-18 12:22:33 +0000

    docker client

--- client/next.config.ts
+++ client/next.config.ts
@@ -18,10 +18,11 @@ const nextConfig: NextConfig = {
     ],
   },
     async rewrites() {
+    const API_URL = process.env.API_URL || 'http://localhost:5000';
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+        destination: `${API_URL}/api/:path*`,
       },
     ];
   },
